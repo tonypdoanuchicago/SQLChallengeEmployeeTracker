6c515865c142821c8b12452da663c02a1d77905d
tonypdoanuchicago <tonypdoan@uchicago.edu>
2024-03-11 19:44:56 -0700

    Final Check In
    Final Check In

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # SQLChallengeEmployeeTracker
  SQL Challenge Employee Tracker
+
+ Description:  This is the read me file for the twelth Challenge assignment for the Web Development Boot Camp at Northwestern.  Requirements implemented and met are found below in grading requirements.
+
+ GitHub Video URL - Go to source file and download to view
+<a href='https://github.com/tonypdoanuchicago/SQLChallengeEmployeeTracker/blob/main/Challenge_12_Video.mov'>Video</a>
+
+GitHub Repo
+https://github.com/tonypdoanuchicago/SQLChallengeEmployeeTracker
+
+This Challenge is graded based on the following criteria:
+
+Deliverables: 10%
+Your GitHub repository containing your application code.
+Walkthrough Video: 27%
+A walkthrough video that demonstrates the functionality of the employee tracker must be submitted, and a link to the video should be included in your README file.
+
+The walkthrough video must show all of the technical acceptance criteria being met.
+
+The walkthrough video must demonstrate how a user would invoke the application from the command line.
+
+The walkthrough video must demonstrate a functional menu with the options outlined in the acceptance criteria.
+
+Technical Acceptance Criteria: 40%
+Satisfies all of the preceding acceptance criteria plus the following:
+
+Uses the Inquirer packageLinks to an external site..
+
+Uses the MySQL2 packageLinks to an external site. to connect to a MySQL database.
+
+Follows the table schema outlined in the homework instructions.
+
+Repository Quality: 13%
+Repository has a unique name.
+
+Repository follows best practices for file structure and naming conventions.
+
+Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
+
+Repository contains multiple descriptive commit messages.
+
+Repository contains a high-quality README with description and a link to a walkthrough video.
+
+Application Quality 10%
+The application user experience is intuitive and easy to navigate.
+Bonus
+Fulfilling any of the following can add up to 20 points to your grade. Note that the highest grade you can achieve is still 100:
+
+Application allows users to update employee managers (2 points).
+
+Application allows users to view employees by manager (2 points).
+
+Application allows users to view employees by department (2 points).
+
+Application allows users to delete departments, roles, and employees (2 points for each).
+
+Application allows users to view the total utilized budget of a department—in other words, the combined salaries of all employees in that department (8 points).
--- index.js
+++ index.js
@@ -3,74 +3,63 @@ import inquirer from 'inquirer';
 
 import mysql from 'mysql';
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-import * as fs from 'fs';
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-      });
-}
+var con;
 
 // TODO: Create a function to initialize app
 function init() {
-  var con = mysql.createConnection({
+  con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "Swabi1234",
-    database: "mydb"
+    database: "hw12"
   });
   
   con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    var sql = "CREATE TABLE IF NOT EXISTS customers (name VARCHAR(255), address VARCHAR(255))";
+    var sql = "CREATE TABLE IF NOT EXISTS department (id INT AUTO_INCREMENT primary key NOT NULL, name VARCHAR(30))";
     con.query(sql, function (err, result) {
       if (err) throw err;
-      console.log("Table created");
+
+      var sql2 = "CREATE TABLE IF NOT EXISTS role (id INT AUTO_INCREMENT primary key NOT NULL, title VARCHAR(30), salary DECIMAL, department_id INT)";
+      con.query(sql2, function (err, result) {
+        if (err) throw err;
+
+        var sql3 = "CREATE TABLE IF NOT EXISTS employee (id INT AUTO_INCREMENT primary key NOT NULL, first_name VARCHAR(30), last_name VARCHAR(30), role_id INT, manager_id INT)";
+        con.query(sql3, function (err, result) {
+          if (err) throw err;
+          showMenu();
+        });
+      });
     });
   });
-  
-  /*
-    inquirer
-  .prompt([
+}
+
+function showMenu() {
+  inquirer.prompt([
     {
-      type: 'maxlength-input',
-        name: 'answerText',
-        message: 'What is the SVG text?',
-        maxLength: 3
-      },
-      {
-        name: 'answerTextColor',
-        message: 'What is the SVG text color?'
-      },
-      {
-        type: 'list',
-        name: 'answerShape',
-        message: 'Which is the purpose of this readme?',
-        choices: ['circle', 'triangle', 'square'],
-      },
-      {
-        name: 'answerShapeColor',
-        message: 'What is the SVG shape color?'
-      }
+      type: 'list',
+      name: 'menuChoice',
+      message: 'What would you like to do?',
+      choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
+    }
   ])
   .then((answers) => {
-    var dataString = "";
-
-    if (answers.answerShape === "circle") {
-        dataString = creaateCircle(answers.answerText, answers.answerTextColor, answers.answerShapeColor);
-    } else if (answers.answerShape === "triangle") {
-      dataString = creaateTriangle(answers.answerText, answers.answerTextColor, answers.answerShapeColor);
-    } else if (answers.answerShape === "square") {
-      dataString = creaateSquare(answers.answerText, answers.answerTextColor, answers.answerShapeColor);
+    if (answers.menuChoice === 'View All Employees') {
+      viewAllEmployees();
+    } else if (answers.menuChoice === 'Add Employee') {
+      addEmployee();
+    } else if (answers.menuChoice === 'Update Employee Role') {
+      updateEmployeeRole();
+    } else if (answers.menuChoice === 'View All Roles') {
+      viewAllRoles();
+    } else if (answers.menuChoice === 'Add Role') {
+      addRole();
+    } else if (answers.menuChoice === 'View All Departments') {
+      viewAllDepartments();
+    } else if (answers.menuChoice === 'Add Department') {
+      addDepartment();
     }
-       
-    writeToFile("logo.svg", dataString);
   })
   .catch((error) => {
     if (error.isTtyError) {
@@ -79,57 +68,295 @@ function init() {
       // Something else went wrong
     }
   });
-
-  */
 }
 
-function creaateCircle(textString, textColor, shapeColor) {
-  var returnString = "<svg width=\"300\" height=\"200\" xmlns=\"http://www.w3.org/2000/svg\">" +
-    "<circle r=\"75\" cx=\"100\" cy=\"100\" fill=\"" + shapeColor + "\" /> " +
-  "<text fill=\"" + textColor + "\" x=\"75\" y=\"75\"" + " " + 
-  " text-anchor=\"middle\"" + " " + 
-  " stroke=\"#0000FF\"" + " " + 
-  " stroke-width=\"1px\"" + " " + 
-  " alignment-baseline=\"middle\"" + " " + 
-  " font-variant=\"all-small-caps\"" + " " + 
-  " font-size=\"25\"" + " " + 
-  " font-weight=\"bold\">" + textString + "</text>" +
-  "</svg>";
+function viewAllEmployees() {
+  var sql = "SELECT * FROM employee";
+  
+  con.query(sql, function (err, result) {
+    if (err) throw err;
 
-  return returnString;
+    console.table(result);
+
+    showMenu();
+  });
 }
 
-function creaateTriangle(textString, textColor, shapeColor) {
-  var returnString = "<div style=\"width:300; height:200;\"><svg height=\"200\" width=\"300\" viewBox=\"0 0 100 100\" xmlns=\"http://www.w3.org/2000/svg\">" +
-    "<polygon points=\"50 15, 100 100, 0 100\" fill=\"" + shapeColor + "\"/>" +
-  "<text fill=\"" + textColor + "\" x=\"50\" y=\"50\"" + " " + 
-  " text-anchor=\"middle\"" + " " + 
-  " stroke=\"#0000FF\"" + " " + 
-  " stroke-width=\"1px\"" + " " + 
-  " alignment-baseline=\"middle\"" + " " + 
-  " font-variant=\"all-small-caps\"" + " " + 
-  " font-size=\"25\"" + " " + 
-  " font-weight=\"bold\">" + textString + "</text>" +
-  "</svg></div>";
+function addEmployee() {
+  var sql = "SELECT * FROM role";
+  
+  con.query(sql, function (err, result) {
+    if (err) throw err;
 
-  return returnString;
+    var json = JSON.stringify(result);
+    
+    var jsonObj = JSON.parse(json);
+
+    var jsonArr = [];
+
+    for (var i = 0; i < jsonObj.length; ++i) {
+      var jsonItem = jsonObj[i];
+
+      jsonArr.push({
+        name: jsonItem.title,
+        value: jsonItem.id
+      });
+    }
+
+    inquirer.prompt([
+    {
+      name: 'firstName',
+      message: 'Enter first name: '
+    },
+    {
+      name: 'lastName',
+      message: 'Enter last name: '
+    },
+    {
+      type: 'list',
+      name: 'roleID',
+      message: 'Which role?',
+      choices: jsonArr
+    },
+    {
+      type: 'list',
+      name: 'managerID',
+      message: 'Which manager?',
+      choices: jsonArr
+    }
+    ])
+    .then((answers) => {
+      var sql = "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('" + answers.firstName + "', '" + answers.lastName + "', " + answers.roleID + ", " + answers.managerID + ") ";
+  
+      con.query(sql, function (err, result) {
+        if (err) throw err;
+
+        showMenu();
+      });  
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+  });
 }
 
-function creaateSquare(textString, textColor, shapeColor) {
-  var returnString = "<svg width=\"300\" height=\"200\" xmlns=\"http://www.w3.org/2000/svg\">" +
-    "<rect width=\"300\" height=\"200\" x=\"10\" y=\"10\" rx=\"20\" ry=\"20\" fill=\"" + shapeColor + "\" /> " +
-    "<text fill=\"" + textColor + "\" x=\"50\" y=\"50\"" + " " + 
-    " text-anchor=\"middle\"" + " " + 
-    " stroke=\"#0000FF\"" + " " + 
-    " stroke-width=\"1px\"" + " " + 
-    " alignment-baseline=\"middle\"" + " " + 
-    " font-variant=\"all-small-caps\"" + " " + 
-    " font-size=\"25\"" + " " + 
-    " font-weight=\"bold\">" + textString + "</text>" + 
-  "</svg>";
+function updateEmployeeRole() {
+  var sql = "SELECT * FROM employee";
+  
+  con.query(sql, function (err, result) {
+    if (err) throw err;
 
-  return returnString;
+    var json = JSON.stringify(result);
+    
+    var jsonObj = JSON.parse(json);
+
+    var jsonArr = [];
+    
+    var employeeID;
+
+    for (var i = 0; i < jsonObj.length; ++i) {
+      var jsonItem = jsonObj[i];
+
+      jsonArr.push({
+        name: jsonItem.first_name + " " + jsonItem.last_name,
+        value: jsonItem.id
+      });
+    }
+
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'employeeID',
+        message: 'Which employee?',
+        choices: jsonArr
+      }
+      ])
+      .then((answers) => {
+        var sql = "SELECT * FROM role";
+  
+        con.query(sql, function (err, result) {
+          if (err) throw err;
+
+          var json2 = JSON.stringify(result);
+          
+          var jsonObj2 = JSON.parse(json2);
+
+          var jsonArr2 = [];
+
+          for (var i = 0; i < jsonObj2.length; ++i) {
+            var jsonItem = jsonObj2[i];
+
+            jsonArr2.push({
+              name: jsonItem.title,
+              value: jsonItem.id
+            });
+          }
+
+          updateEmployeRolePrompt(jsonArr2, answers.employeeID);
+        }); 
+      })
+      .catch((error) => {
+        if (error.isTtyError) {
+          // Prompt couldn't be rendered in the current environment
+        } else {
+          // Something else went wrong
+        }
+      });
+});
 }
+
+function updateEmployeRolePrompt(jsonArr, employeeID) {
+  inquirer.prompt([
+    {
+      name: 'firstName',
+      message: 'Enter first name: '
+    },
+    {
+      name: 'lastName',
+      message: 'Enter last name: '
+    },
+    {
+      type: 'list',
+      name: 'roleID',
+      message: 'Which role?',
+      choices: jsonArr
+    },
+    {
+      type: 'list',
+      name: 'managerID',
+      message: 'Which manager?',
+      choices: jsonArr
+    }
+    ])
+    .then((answers) => {
+      var sql = "UPDATE employee SET first_name = '" + answers.firstName + "' , last_name = '" + answers.lastName + "', role_id = " + answers.roleID + ", manager_id = " + answers.managerID + 
+        " WHERE id = " + employeeID;
+  
+      con.query(sql, function (err, result) {
+        if (err) throw err;
+
+        showMenu();
+      });  
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+}
+
+function viewAllRoles() {
+  var sql = "SELECT * FROM role";
+  
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+
+    console.table(result);
+
+    showMenu();
+  });
+}
+
+function addRole() {
+  var sql = "SELECT * FROM department";
+  
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+
+    var json = JSON.stringify(result);
+    
+    var jsonObj = JSON.parse(json);
+
+    var jsonArr = [];
+
+    for (var i = 0; i < jsonObj.length; ++i) {
+      var jsonItem = jsonObj[i];
+
+      jsonArr.push({
+        name: jsonItem.name,
+        value: jsonItem.id
+      });
+    }
+
+    inquirer.prompt([
+    {
+      name: 'title',
+      message: 'Enter title: '
+    },
+    {
+      name: 'salary',
+      message: 'Enter salary: '
+    },
+    {
+      type: 'list',
+      name: 'departmentID',
+      message: 'Which department?',
+      choices: jsonArr
+    }
+    ])
+    .then((answers) => {
+      var sql = "INSERT INTO role (title, salary, department_id) VALUES ('" + answers.title + "', " + answers.salary + ", " + answers.departmentID + ") ";
+  
+      con.query(sql, function (err, result) {
+        if (err) throw err;
+
+        showMenu();
+      });  
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+  });
+}
+
+function viewAllDepartments() {
+  var sql = "SELECT * FROM department";
+  
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+
+    console.table(result);
+
+    showMenu();
+  });
+}
+
+function addDepartment() {
+  inquirer.prompt([
+    {
+      name: 'departmentName',
+      message: 'Enter department name?'
+    }
+  ])
+  .then((answers) => {
+    
+    var sql = "INSERT INTO department (name) VALUES ('" + answers.departmentName + "') ";
+  
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+
+      showMenu();
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+}
+
 
 // Function call to initialize app
 init();
